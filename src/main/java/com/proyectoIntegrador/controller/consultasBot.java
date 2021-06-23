fc0a7f1cf261a17fe0aa82187666238ad99e49c6
DanielChr18 <DanielChr18@outlook.com>
2021-06-23 15:36:44 -0500

    segunda prueba para el chat bot

--- src/main/java/com/proyectoIntegrador/controller/consultasBot.java
+++ src/main/java/com/proyectoIntegrador/controller/consultasBot.java
@@ -2,10 +2,12 @@ package com.proyectoIntegrador.controller;
 
 import java.util.List;
 
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.google.gson.Gson;
 import com.proyectoIntegrador.entity.Producto;
 import com.proyectoIntegrador.service.ProductoService;
 
@@ -15,11 +17,19 @@ public class consultasBot {
 	@Autowired
 	private ProductoService service;
 
-	@GetMapping(produces = "application/json", value = "/consultaProductoBot")
-	public List<Producto> listadoProductosNombre(String nombreArticulo) {
-		System.out.println("Listar Productos por Nombre : Filtro -----> " + nombreArticulo);
-		List<Producto> lista = service.listaProductosNombre("%" + nombreArticulo + "%");
-		return lista;
+	@GetMapping(produces = "application/json;charset=UTF-8", value = "/consultaProductoBot")
+	public String listadoProductosNombre() {
+		List<Producto> lista = service.listaProductosNombre("%" + "r" + "%");
+		Gson gson = new Gson();
+		JSONObject jsonObject = new JSONObject();
+		String nombres = "";
+		for (Producto x : lista) {
+			nombres += x.getDescripcion() + ",";
+		}
+		if (nombres.length() != 0)
+			jsonObject.put("nombres", nombres.substring(0, nombres.length() - 1));
+		String jsonInString = gson.toJson(jsonObject);
+		return jsonInString;
 	}
 
 }
