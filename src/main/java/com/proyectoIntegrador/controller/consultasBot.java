1da2514c3b8557c1e135bae7173ab0218c89eeef
DanielChr18 <DanielChr18@outlook.com>
2021-06-23 18:04:59 -0500

    Prueba 2 para el chat

--- src/main/java/com/proyectoIntegrador/controller/consultasBot.java
+++ src/main/java/com/proyectoIntegrador/controller/consultasBot.java
@@ -1,9 +1,14 @@
 package com.proyectoIntegrador.controller;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.proyectoIntegrador.entity.Producto;
 import com.proyectoIntegrador.service.ProductoService;
 
 @RestController
@@ -13,7 +18,18 @@ public class consultasBot {
 	private ProductoService service;
 
 	@GetMapping(produces = "application/json;charset=UTF-8", value = "/consultaProductoBot")
-	public String listadoProductosNombre() {
-		return "";
+	public Map<String, Object> listadoProductosNombre() {
+		Map<String, Object> json = new HashMap<>();
+		List<Producto> lista = service.listaProductosNombre("%" + "r" + "%");
+		String nombres = "";
+		for (Producto x : lista) {
+			nombres += x.getDescripcion() + ",";
+		}
+
+		if (nombres.length() != 0)
+			json.put("nombres", nombres.substring(0, nombres.length() - 1));
+		System.out.println(json);
+		return json;
 	}
+
 }
